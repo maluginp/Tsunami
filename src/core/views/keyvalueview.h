f21a0c16df8c97baee3834c6c95a4cc7c0f87801
Malyugin Platon <malugin.p@gmail.com>
2013-09-20 13:52:50 +0700

    have done keyvalueview

--- src/core/views/keyvalueview.h
+++ src/core/views/keyvalueview.h
@@ -7,6 +7,11 @@ struct KeyValuePair{
     QString  key;
     QVariant value;
     KeyValueView::ValueType type;
+
+    KeyValuePair() :
+        key(QString()),value(QVariant()),type(KeyValueView::TYPE_TEXT) {}
+    KeyValuePair(const KeyValuePair& pair):
+        key(pair.key),value(pair.value),type(pair.type) {}
 };
 
 class KeyValueView : public QAbstractItemModel
@@ -20,12 +25,29 @@ public:
     };
 
     explicit KeyValueView(QObject *parent = 0);
-//    void setValues(const QVariantMap& keyValues);
+
+    // Model
+    QModelIndex index(int row, int column,
+                      const QModelIndex &parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex &child) const;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+    QVariant headerData ( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
+
+
+
+    // API
+    void addPairs( const KeyValuePair* pairs,  const int& num );
     void addPair( const QString& key, const QVariant& value, const ValueType& type );
     void setValue( const QString& key, const QVariant& value );
-
+    void setPair( const QString& key, const KeyValuePair& pair );
 
     const QList<KeyValuePair>& getPairs() const { return keyValues_; }
+    const KeyValuePair& getPair( const int& index ) const;
+
 
 signals:
     
