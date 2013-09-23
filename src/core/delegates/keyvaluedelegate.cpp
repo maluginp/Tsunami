f215479964a700ac613c83dcb92d6a2a79b80945
Malyugin Platon <malugin.p@gmail.com>
2013-09-23 10:59:51 +0700

    make skel for keyvaluedelegate

--- src/core/delegates/keyvaluedelegate.cpp
+++ src/core/delegates/keyvaluedelegate.cpp
@@ -1,5 +1,72 @@
 #include "keyvaluedelegate.h"
-
-KeyValueDelegate::KeyValueDelegate()
-{
+#include <QComboBox>
+KeyValueDelegate::KeyValueDelegate(const QList<KeyValuePair> &pairs, QObject *parent) {
+    pairs_ = pairs;
 }
+
+QWidget *KeyValueDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+    if( index.column() == 1 && index.row() < pairs_.size() ){
+        KeyValuePair pair = pairs_[index.row()];
+
+        if(pair.type == KeyValuePair::TYPE_LIST){
+            QComboBox* editor = new QComboBox(parent);
+            QVariantMap items = pair.data.toMap();
+
+            foreach(QString key,items.keys()){
+                editor->addItem( key, items.value( key ) );
+            }
+            return editor;
+        }
+    }
+
+    return QItemDelegate::createEditor(parent,option,index);
+}
+
+void KeyValueDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const {
+    if( index.column() == 1 && index.row() < pairs_.size() ){
+        KeyValuePair pair = pairs_[index.row()];
+
+        if(pair.type == KeyValuePair::TYPE_LIST){
+            QComboBox *comboBox = static_cast<QComboBox*>(editor);
+            QVariant value  = index.model()->data(index, Qt::EditRole);
+            comboBox->setCurrentIndex(  itemIndex(value)  );
+        }
+    }
+}
+
+void KeyValueDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const {
+    if( index.column() == 1 && index.row() < pairs_.size() ){
+        KeyValuePair pair = pairs_[index.row()];
+        if(pair.type == KeyValuePair::TYPE_LIST){
+            QComboBox *comboBox = static_cast<QComboBox*>(editor);
+            //            QString key = items_.key(comboBox->itemData( comboBox->currentIndex(), Qt::UserRole ));
+
+            model->setData(index, comboBox->itemData( comboBox->currentIndex(), Qt::UserRole ), Qt::DisplayRole );
+        }
+    }
+
+}
+
+void KeyValueDelegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+    if( index.column() == 1 && index.row() < pairs_.size() ){
+        KeyValuePair pair = pairs_[index.row()];
+        if(pair.type == KeyValuePair::TYPE_LIST){
+            editor->setGeometry(option.rect);
+        }
+    }
+}
+
+void KeyValueDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+    if( index.column() == 1 && index.row() < pairs_.size() ){
+        KeyValuePair pair = pairs_[index.row()];
+        if(pair.type == KeyValuePair::TYPE_LIST){
+
+            QStyleOptionViewItemV4 myOption = option;
+            QString text = items_.key(index.data(Qt::DisplayRole));
+            myOption.text = text;
+            QApplication::style()->drawControl(QStyle::CE_ItemViewItem, &myOption, painter);
+
+        }
+    }
+}
+
--- src/core/delegates/keyvaluedelegate.h
+++ src/core/delegates/keyvaluedelegate.h
@@ -2,11 +2,15 @@
 #define KEYVALUEDELEGATE_H
 
 #include <QItemDelegate>
+#include "../views/keyvalueview.h"
 
 class KeyValueDelegate : public QItemDelegate
 {
 public:
-    KeyValueDelegate();
+    KeyValueDelegate(const QList<KeyValuePair>& pairs, QObject *parent = 0);
+
+private:
+    QList<KeyValuePair> pairs_;
 };
 
 #endif // KEYVALUEDELEGATE_H
