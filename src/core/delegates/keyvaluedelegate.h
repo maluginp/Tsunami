f209d42635c8119d6fd5fe7291cb0678dd79c4e1
Malyugin Platon <malugin.p@gmail.com>
2013-09-23 11:06:48 +0700

    fixed header for keyvaluedelegate

--- src/core/delegates/keyvaluedelegate.h
+++ src/core/delegates/keyvaluedelegate.h
@@ -8,7 +8,13 @@ class KeyValueDelegate : public QItemDelegate
 {
 public:
     KeyValueDelegate(const QList<KeyValuePair>& pairs, QObject *parent = 0);
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    void setEditorData(QWidget *editor, const QModelIndex &index) const;
+    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
+    void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
 
+    int itemIndex( const QVariant& value ) const;
 private:
     QList<KeyValuePair> pairs_;
 };
